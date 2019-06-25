9d55d8cded860839f5dac1d9689f11a97153cbec
lourdesvega <39252591+lourdesvega@users.noreply.github.com>
2019-06-24 21:56:45 -0500

    ya

--- app/src/main/java/com/example/variousobjects/MainActivity.java
+++ app/src/main/java/com/example/variousobjects/MainActivity.java
@@ -24,22 +24,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick (View view)
     {
 
-        EditText correo = findViewById(R.id.correo);
-        EditText contrasena = findViewById(R.id.password);
-
-        String valorCorreo = correo.getText().toString();
-        String valorContrasena = contrasena.getText().toString();
-
-        if ((valorCorreo.equals("omar.blanco@8w.com.mx")||valorCorreo.equals("lourdesvega21@hotmail.com")||valorCorreo.equals("djdaan86@gmail.com "))&&valorContrasena.equals("123456"))
-        {
-            intent = new Intent(this, Menu.class);
-            finish();
-            startActivity(intent);
-            Toast.makeText(this,"Abriendo Segunda Actividad", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Usuario y/o Contraseña incorrecto", Toast.LENGTH_SHORT).show();
-        }
-
 
         switch (view.getId()){
             case R.id.ingresar:
@@ -65,4 +49,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-}
+}}
